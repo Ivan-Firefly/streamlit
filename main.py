3827e4f330d3dcd97c5e2496bd32fed830cb1bd5
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2024-09-18 10:18:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ st.title('ECHO')
 def func():
     echo= input("Type something:")
     if echo != '':
-        print('You typed:'+echo)
+        returnt print('You typed:'+echo)
     else:
-        print('Input string is empty')
+        returnt print('Input string is empty')
 
 st.write(func)
 
