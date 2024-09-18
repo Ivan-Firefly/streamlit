8baad644c7e4cafe9c5b46b78b0fc16467570e5a
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2024-09-18 10:06:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-echo= input("Type something:")
+import streamlit as st
+
+
+@st.cache_data
+def func():
+    echo= input("Type something:")
 
 if echo != '':
     print('You typed:'+echo)
 else:
     print('Input string is empty')
+
