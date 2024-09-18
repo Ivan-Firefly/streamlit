b03c6f158da83d3d6abd88cb6ee759216151405e
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2024-09-18 10:54:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 st.title('ECHO')
 
-def func():
-    echo= input("Type something:")
-    if echo != '':
-        output='You typed:'+echo
-    else:
-        output='Input string is empty'
-    return output
 
-st.write(func())
+echo=st.text_input("Type something:", key="sting")
+if echo != '':
+    output='You typed:'+echo
+else:
+    output='Input string is empty'
+
+
+st.write(output)
